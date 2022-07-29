9e33657d3b84471e83db88e7385e6a399a6ae0d2
MColbrook <mjc249@cam.ac.uk>
2022-07-29 17:08:02 +0200

    Psuedoeigenfunction capability added

--- main_routines/KoopPseudoSpec.m
+++ main_routines/KoopPseudoSpec.m
@@ -1,4 +1,4 @@
-function [RES] = KoopPseudoSpec(G,A,L,z_pts,varargin)
+function [RES,RES2,V2] = KoopPseudoSpec(G,A,L,z_pts,varargin)
 % This code computes pseudospectrum of K (currently written for dense matrices).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % INPUTS
@@ -9,10 +9,14 @@ function [RES] = KoopPseudoSpec(G,A,L,z_pts,varargin)
 
 % OPTIONAL LABELLED INPUTS
 % parallel: parfor (on) or normal for (off) loop, default is "off"
+% z_pts2: vector of complex points where we want to compute
+% pseudoeigenfunctions
 % reg_param: regularisation parameter for G
 
 % OUTPUTS
 % RES: residual for shifts z_pts.
+% RES2: residual for pseudoeigenfunctions corresponding to shifts z_pts2.
+% RES2: pseudoeigenfunctions corresponding to shifts z_pts2.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Collect the optional inputs
@@ -26,6 +30,7 @@ validPar = {'on','off'};
 checkPar = @(x) any(validatestring(x,validPar));
 
 addParameter(p,'Parallel','off',checkPar)
+addParameter(p,'z_pts2',[],@isnumeric)
 addParameter(p,'reg_param',10^(-14),@isnumeric)
 
 p.CaseSensitive = false;
@@ -41,20 +46,45 @@ z_pts=z_pts(:);
 LL=length(z_pts);
 RES=zeros(LL,1);
 
-warning('off','all')
-
-pf = parfor_progress(LL);
-pfcleanup = onCleanup(@() delete(pf));
-if p.Results.Parallel=="on"
-    parfor jj=1:LL
-        warning('off','all')
-        RES(jj)=sqrt(min(eigs( SQ*((L)-z_pts(jj)*A'-conj(z_pts(jj))*A+(abs(z_pts(jj))^2)*G)*SQ,1,'smallestabs')));
-        parfor_progress(pf);
+if LL>0
+    warning('off','all')
+    pf = parfor_progress(LL);
+    pfcleanup = onCleanup(@() delete(pf));
+    if p.Results.Parallel=="on"
+        parfor jj=1:LL
+            warning('off','all')
+            RES(jj)=sqrt(real(eigs( SQ*((L)-z_pts(jj)*A'-conj(z_pts(jj))*A+(abs(z_pts(jj))^2)*G)*SQ,1,'smallestabs')));
+            parfor_progress(pf);
+        end
+    else
+        for jj=1:LL
+            RES(jj)=sqrt(real(eigs( SQ*((L)-z_pts(jj)*A'-conj(z_pts(jj))*A+(abs(z_pts(jj))^2)*G)*SQ,1,'smallestabs')));
+            parfor_progress(pf);
+        end
     end
-else
-    for jj=1:LL
-        RES(jj)=sqrt(min(eigs( SQ*((L)-z_pts(jj)*A'-conj(z_pts(jj))*A+(abs(z_pts(jj))^2)*G)*SQ,1,'smallestabs')));
-        parfor_progress(pf);
+end
+
+RES2=[];
+V2=[];
+
+if ~isempty(p.Results.z_pts2)
+    RES2=zeros(length(p.Results.z_pts2),1);
+    V2=zeros(size(G,1),length(p.Results.z_pts2));
+    pf = parfor_progress(length(p.Results.z_pts2));
+    pfcleanup = onCleanup(@() delete(pf));
+    if p.Results.Parallel=="on"
+        parfor jj=1:length(p.Results.z_pts2)
+            warning('off','all')
+            [V,D]=eigs( SQ*((L)-p.Results.z_pts2(jj)*A'-conj(p.Results.z_pts2(jj))*A+(abs(p.Results.z_pts2(jj))^2)*G)*SQ,1,'smallestabs');
+            V2(:,jj)=V; RES2(jj)=sqrt(real(D(1,1)));
+            parfor_progress(pf);
+        end
+    else
+        for jj=1:length(p.Results.z_pts2)
+            [V,D]=eigs( SQ*((L)-p.Results.z_pts2(jj)*A'-conj(p.Results.z_pts2(jj))*A+(abs(p.Results.z_pts2(jj))^2)*G)*SQ,1,'smallestabs');
+            V2(:,jj)=V; RES2(jj)=sqrt(real(D(1,1)));
+            parfor_progress(pf);
+        end
     end
 end
 
