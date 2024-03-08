d45141f7c8e61352fd01e6df0a1580b1ab20f4b2
Matthew Colbrook <mjc249@cam.ac.uk>
2024-03-08 21:38:32 +0000

    Update van_der_pol_example.m
    New kernelResDMD convention.

--- Examples_gallery_3/van_der_pol_example.m
+++ Examples_gallery_3/van_der_pol_example.m
@@ -5,7 +5,7 @@ load('van_der_pol_data') % load the trajectory data from here: https://www.dropb
 
 
 % %% Compute ResDMD matrices
-% [PX,PY1,PY2] = kernel_ResDMD(Xa,Ya,Xb,Yb,'Y2',Y2,'parallel','off','N',N);
+% [~,~,~,~,~,PX,PY1,PY2] = kernel_ResDMD(Xa,Ya,'Xb',Xb,'Yb',Yb,'Y2',Y2,'N',N);
 % %%
 % G = (PX'*PX)/M;
 % A = PX'*(PY1+PY2)/(2*M);
