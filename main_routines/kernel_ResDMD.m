9295af819d55a056dfe79b5cd49329b7a7a4b264
Matthew Colbrook <mjc249@cam.ac.uk>
2024-03-13 12:19:12 +0000

    Update kernel_ResDMD.m
    Much faster code and benefit of new residuals.

--- main_routines/kernel_ResDMD.m
+++ main_routines/kernel_ResDMD.m
@@ -1,155 +1,96 @@
-function [PSI_x,PSI_y,PSI_y2] = kernel_ResDMD(Xa,Ya,Xb,Yb,varargin)
+function [G,K,L,PX,PY,PSI_x,PSI_y,PSI_y2] = kernel_ResDMD(Xa,Ya,varargin)
 % This code applies kernelized ResDMD.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % INPUTS
 % Xa and Ya: data matrices used in kernel_EDMD to form dictionary (columns
 % correspond to instances of the state variable)
-% Xb, Yb: data matrices used in ResDMD
 
 % OPTIONAL LABELLED INPUTS
 % N: size of computed dictionary, default is number of data points for kernel EDMD
-% kernel_f: kernel used, default is normalised Gaussian
-% Parallel: parfor (on) or normal for (off) loop, default is "off"
-% Sketch: sketching to approximate Gaussian kernel, default is "off"
-% s: size of sketching
-% cut_off: stability parameter for SVD, default is 10^(-12)
+% type: kernel used, default is normalised Gaussian, "Laplacian" is for
+% nomralised Laplacian, and numeric value (e.g., 20) is for polynomial
+% kernel
+% cut_off: stability parameter for SVD, default is 0
+% Xb, Yb: additional data matrices used in ResDMD for test data
 % Y2: additional data matrix for stochastic version
 
 % OUTPUTS
+% G K L matrices for kernelResDMD
 % PSI matrices for ResDMD
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Collect the optional inputs
 p = inputParser;
 
-M1=size(Xa,2);
-M2=size(Xb,2);
-d=mean(vecnorm(Xa-mean(Xa')'));
-
-validPar = {'on','off'};
-checkPar = @(x) any(validatestring(x,validPar));
-validSketch = {'on','off'};
-checkSketch = @(x) any(validatestring(x,validSketch));
-
 addParameter(p,'N',size(Xa,2),@(x) x==floor(x))
-addParameter(p,'kernel_f',@(x,y) exp(-vecnorm(x-y)/d))
-addParameter(p,'Parallel','off',checkPar)
-addParameter(p,'Sketch','off',checkSketch)
-addParameter(p,'cut_off',10^(-12),@(x) x>=0)
-addParameter(p,'s',max(ceil(5*sqrt(M1+M2)*log(M1+M2)),5000),@(x) x==floor(x))
+addParameter(p,'type',"Gaussian");
+addParameter(p,'cut_off',0,@(x) x>=0)
+addParameter(p,'Xb',[],@isnumeric)
+addParameter(p,'Yb',[],@isnumeric)
 addParameter(p,'Y2',[],@isnumeric)
 
 p.CaseSensitive = false;
 parse(p,varargin{:})
 
 % Apply kernel EDMD
-
-if p.Results.Sketch=="off"
-    G1=zeros(M1,M1); A1=G1;
-    kernel_f=p.Results.kernel_f;
-    pf = parfor_progress(M1); pfcleanup = onCleanup(@() delete(pf));
-    if p.Results.Parallel=="off"
-        for i=1:M1
-            G1(i,:)=kernel_f(Xa(:,i),Xa);
-            A1(i,:)=kernel_f(Ya(:,i),Xa);
-            parfor_progress(pf);
-        end
+if isnumeric(p.Results.type)
+    d = mean(vecnorm(Xa));
+    kernel_f = @(x,y) (y'*x/d^2+1).^(p.Results.type);
+elseif p.Results.type=="Linear"
+    kernel_f = @(x,y) y'*x;
+elseif p.Results.type=="Laplacian"
+    d = mean(vecnorm(Xa-mean(Xa,2)));
+    if isa(Xa,'single') % safeguard against square root (but a little bit slower)
+        kernel_f = @(x,y) exp(-pdist2(y',x')/d);
     else
-        parfor i=1:M1
-            G1(i,:)=kernel_f(Xa(:,i),Xa);
-            A1(i,:)=kernel_f(Ya(:,i),Xa);
-            parfor_progress(pf);
-        end
+        kernel_f = @(x,y) exp(-sqrt(-2*real(y'*x)+dot(x,x)+dot(y,y)')/d);
     end
-else
-    s=p.Results.s;
-    Z=sqrt(2/d^2)*randn([size(Xa,1),s]);
-    TH=2*pi*rand(s,1);
-    
-    psi_xa=sqrt(2/s)*cos(TH+Z'*Xa);
-    psi_ya=sqrt(2/s)*cos(TH+Z'*Ya);
-
-    G1=psi_xa'*psi_xa;
-    A1=psi_ya'*psi_xa;
-    G1=max(G1,zeros(size(G1)));
-    A1=max(A1,zeros(size(A1)));
+elseif p.Results.type=="Gaussian"
+    d = mean(vecnorm(Xa-mean(Xa,2)));
+    kernel_f = @(x,y) exp(-(-2*real(y'*x)+dot(x,x)+dot(y,y)')/d^2);
+elseif p.Results.type=="Lorentzian"
+    d = mean(vecnorm(Xa-mean(Xa,2)));
+    kernel_f = @(x,y) (1+(-2*real(y'*x)+dot(x,x)+dot(y,y)')/d^2).^(-1);
 end
+
+G1 = kernel_f(Xa,Xa); G1 = (G1+G1')/2;
+A1 = kernel_f(Ya,Xa)';
+L1 = kernel_f(Ya,Ya);  L1 = (L1+L1')/2;
 
 % Post processing
 
 [U,D0] = eig(G1+norm(G1)*p.Results.cut_off*eye(size(G1)));
-D0(D0<p.Results.cut_off)=0;
-SIG=sqrt(D0); SIG_dag = 0*SIG; SIG_dag(SIG>0)=1./(SIG(SIG>0));
+[~,I] = sort(diag(D0),'descend');
+U = U(:,I); D0 = D0(I,I);
+N = min(p.Results.N,length(find(diag(D0)>0)));
+U = U(:,1:N); D0 = D0(1:N,1:N);
+UU = U*sqrt(diag(1./diag(D0)));
 
-K_hat = SIG_dag*U'*A1*U*SIG_dag;
-[U1,D1] = eig(K_hat);
+% G = UU'*G1*UU;
+G = eye(N);
+K = UU'*A1*UU;
+L = UU'*L1*UU;
 
-I=find(abs(diag(D1))>p.Results.cut_off);
-if length(I)>p.Results.N
-    [~,I]=sort(abs(diag(D1)),'descend');
-    N=p.Results.N;
+PX = G1'*UU;
+PY = A1*UU;
+
+if ~isempty(p.Results.Xb) % test data case
+    PSI_x = kernel_f(p.Results.Xb,Xa)'*UU;
 else
-    N=length(I);
+    PSI_x =[];
 end
 
-[P,~,~]=svd(U1(:,I(1:N)),0);
-P=U*SIG_dag*P;
-
-% Compute matrices for ResDMD
-
-if p.Results.Sketch=="off"
-    PSI_x=zeros(M2,M1); PSI_y=zeros(M2,M1);
-    if ~isempty(p.Results.Y2) % stochastic case
-        PSI_y2=zeros(M2,M1);
-    end
-    kernel_f=p.Results.kernel_f;
-
-    pf = parfor_progress(M2); pfcleanup = onCleanup(@() delete(pf));
-    if p.Results.Parallel=="off"
-        for ii=1:M2
-            PSI_x(ii,:)=kernel_f(Xb(:,ii),Xa);
-            PSI_y(ii,:)=kernel_f(Yb(:,ii),Xa);
-            if ~isempty(p.Results.Y2) % stochastic case
-                PSI_y2(ii,:)=kernel_f(p.Results.Y2(:,ii),Xa);
-            end
-            parfor_progress(pf);
-        end
-    else
-        parfor ii=1:M2
-            PSI_x(ii,:)=kernel_f(Xb(:,ii),Xa);
-            PSI_y(ii,:)=kernel_f(Yb(:,ii),Xa);
-            if ~isempty(p.Results.Y2) % stochastic case
-                PSI_y2(ii,:)=kernel_f(p.Results.Y2(:,ii),Xa);
-            end
-            parfor_progress(pf);
-        end
-    end
-
-    PSI_x=PSI_x*P;
-    PSI_y=PSI_y*P;
-    if ~isempty(p.Results.Y2) % stochastic case
-        PSI_y2=PSI_y2*P;
-    end
+if ~isempty(p.Results.Yb) % test data case
+    PSI_y = kernel_f(p.Results.Yb,Xa)'*UU;
 else
-    psi_xb=sqrt(2/s)*cos(TH+Z'*Xb);
-    psi_yb=sqrt(2/s)*cos(TH+Z'*Yb);
-    
-    PSI_x=psi_xb'*psi_xa;
-    PSI_y=psi_yb'*psi_xa;
-    
-    PSI_x=max(PSI_x,zeros(size(PSI_x)))*P;
-    PSI_y=max(PSI_y,zeros(size(PSI_y)))*P;
-    
-    if ~isempty(p.Results.Y2) % stochastic case
-        psi_y2b=sqrt(2/s)*cos(TH+Z'*p.Results.Y2);
-        PSI_y2=psi_y2b'*psi_xa;
-        PSI_y2=max(PSI_y2,zeros(size(PSI_y2)))*P;
-    end
-
+    PSI_y =[];
 end
 
-if isempty(p.Results.Y2)
-    PSI_y2=[];
+if ~isempty(p.Results.Y2) % stochastic case
+    PSI_y2 = kernel_f(p.Results.Y2,Xa)'*UU;
+else
+    PSI_y2 =[];
 end
+
 
 end
